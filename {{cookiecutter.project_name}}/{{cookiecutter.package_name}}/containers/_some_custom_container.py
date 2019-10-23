6649845b33a00f40ffc1160b081cd72063f95de6
Anders Fredrik Kiær <akia@statoil.com>
2019-10-23 13:22:56 +0200

    linting of python template code

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/containers/_some_custom_container.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/containers/_some_custom_container.py
@@ -3,10 +3,8 @@ from webviz_config import WebvizContainerABC
 
 
 class SomeCustomContainer(WebvizContainerABC):
-
     @property
     def layout(self):
-        return html.Div([
-                         html.H1('This is a static title'),
-                         'And this is just some ordinary text'
-                        ])
+        return html.Div(
+            [html.H1("This is a static title"), "And this is just some ordinary text"]
+        )
--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/containers/_some_other_custom_container.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/containers/_some_other_custom_container.py
@@ -6,24 +6,25 @@ from webviz_config import WebvizContainerABC
 
 
 class SomeOtherCustomContainer(WebvizContainerABC):
-
     def __init__(self, app):
-        self.button_id = f'submit-button-{uuid4()}'
-        self.div_id = f'output-state-{uuid4()}'
+        self.button_id = f"submit-button-{uuid4()}"
+        self.div_id = f"output-state-{uuid4()}"
 
         self.set_callbacks(app)
 
     @property
     def layout(self):
-        return html.Div([
-                         html.H1('This is a static title'),
-                         html.Button(id=self.button_id, n_clicks=0,
-                                     children='Submit'),
-                         html.Div(id=self.div_id)
-                        ])
+        return html.Div(
+            [
+                html.H1("This is a static title"),
+                html.Button(id=self.button_id, n_clicks=0, children="Submit"),
+                html.Div(id=self.div_id),
+            ]
+        )
 
     def set_callbacks(self, app):
-        @app.callback(Output(self.div_id, 'children'),
-                      [Input(self.button_id, 'n_clicks')])
+        @app.callback(
+            Output(self.div_id, "children"), [Input(self.button_id, "n_clicks")]
+        )
         def _update_output(n_clicks):
-            return f'Button has been pressed {n_clicks} times.'
+            return f"Button has been pressed {n_clicks} times."
