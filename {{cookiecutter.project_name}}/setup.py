224523b89472051e23053858cc16b349bb2fd126
Anders Fredrik Kiær <akia@statoil.com>
2020-04-29 12:06:34 +0200

    Add python_requires

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -22,6 +22,7 @@ setup(
     tests_require=TESTS_REQUIRE,
     extras_require={"tests": TESTS_REQUIRE},
     setup_requires=["setuptools_scm~=3.2"],
+    python_requires="~=3.6",
     use_scm_version=True,
     zip_safe=False,
     classifiers=[
